a30c25001564bb0954ea6fef7522756aaf3670ac
Abusiddik101 <abubakker549@gmail.com>
2021-12-28 20:51:15 -0500

    Home

---
+++ src/Home.java
@@ -0,0 +1,20 @@
+public class Home {
+
+
+    // static
+    // non static
+
+
+    static String name;
+    static String address;
+
+    public void getName(){
+        System.out.println(name);
+    }
+    public void getAddress(){
+        System.out.println(address);
+
+    }
+
+
+}
