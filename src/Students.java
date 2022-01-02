cc6062a4412aefc4b186c91bb0bda52ff66c007e
Abusiddik101 <abubakker549@gmail.com>
2022-01-01 20:45:09 -0500

    myfirstcommit

--- src/Students.java
+++ src/Students.java
@@ -1,60 +1,60 @@
-public class Students { //box
-
-
-    public static void main(String[] args) {
-
-        String name = "David"; //small box
-        int id = 102; // primitive
-        double gpa = 3.5;
-        boolean isCurrentStudent = true;
-
-
-        acSpe MethodNameIsNameAsClass () {
-            public Students(String name) {
-                private String name =
-                        "David";
-                private int id = 102;
-
-
-// Class = is A blueprint of an object
-// Object = Object is an instance of a class
-
-public Students(String name3) {
-                    7
-                    public String studentName (String name4){
-                        String namel
-                        "adam";
-                        return namel;
-                        7
-                        public int studentID0 {
-                            System.out.println(name);
-                            return id;
-                            public void studentSection () {
-                                System.out.println("This is a student Section method")
-
-
-
-                        System.out.println(name);
-                        System.out.println(id);
-                        System.out.println(gpa);
-
-
-                    }
-
-
-                    Students std = new Students();
-                    std.studentName();
-                    std.studentID();
-                    std.studentSection();
-
-
-                    Home home = Home();
-
-                    Home. getaddress(); not belongs to object
-                    Home. getName
-
-                    Home.name = "Jhon";
-                    Home. address = "203 adam street";
-
-
-                }
+//public class Students { //box
+//
+//
+//    public static void main(String[] args) {
+//
+//        String name = "David"; //small box
+//        int id = 102; // primitive
+//        double gpa = 3.5;
+//        boolean isCurrentStudent = true;
+//
+//
+//        acSpe MethodNameIsNameAsClass () {
+//            public Students(String name) {
+//                private String name =
+//                        "David";
+//                private int id = 102;
+//
+//
+//// Class = is A blueprint of an object
+//// Object = Object is an instance of a class
+//
+//public Students(String name3) {
+//                    7
+//                    public String studentName (String name4){
+//                        String namel
+//                        "adam";
+//                        return namel;
+//                        7
+//                        public int studentID0 {
+//                            System.out.println(name);
+//                            return id;
+//                            public void studentSection () {
+//                                System.out.println("This is a student Section method")
+//
+//
+//
+//                        System.out.println(name);
+//                        System.out.println(id);
+//                        System.out.println(gpa);
+//
+//
+//                    }
+//
+//
+//                    Students std = new Students();
+//                    std.studentName();
+//                    std.studentID();
+//                    std.studentSection();
+//
+//
+//                    Home home = Home();
+//
+//                    Home. getaddress(); not belongs to object
+//                    Home. getName
+//
+//                    Home.name = "Jhon";
+//                    Home. address = "203 adam street";
+//
+//
+//                }
